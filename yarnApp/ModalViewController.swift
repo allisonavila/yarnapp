edc841addbabfa57be8bc7b0d6d9829586d2f8df
Allison Avila Vidal <alli.andrea17@gmail.com>
2022-06-16 20:06:31 -0500

    Modificacion

---
+++ yarnApp/ModalViewController.swift
@@ -0,0 +1,33 @@
+//
+//  ModalViewController.swift
+//  yarnApp
+//
+//  Created by Allison Avila Vidal on 16/06/22.
+//
+
+import UIKit
+
+class ModalViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+            
+        view.backgroundColor = .blue
+        
+
+        
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
